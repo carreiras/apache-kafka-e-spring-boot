a500e55d94ea36e920eac36302f1c0260751c4b1
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-05-08 17:19:33 -0300

    Implementando Kakfa Client

---
+++ shop-api/src/main/java/carreiras/com/github/shopapi/kafka/KafkaClient.java
@@ -0,0 +1,18 @@
+package carreiras.com.github.shopapi.kafka;
+
+import carreiras.com.github.shopapi.dto.ShopDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaClient {
+
+    private final KafkaTemplate<String, ShopDTO> kafkaTemplate;
+    private static final String SHOP_TOPIC_NAME = "SHOP_TOPIC";
+
+    public void sendMessage(ShopDTO msg) {
+        kafkaTemplate.send(SHOP_TOPIC_NAME, msg);
+    }
+}
